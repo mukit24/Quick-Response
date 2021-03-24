5e1fe3fb871526ffe499edaadc136f95bc0c7b61
mukit hasan pranto <mukithasanpranto4@gmail.com>
2021-03-24 13:29:55 +0600

    problems_sort

--- problems/urls.py
+++ problems/urls.py
@@ -3,4 +3,8 @@ from .views import prob_home,create_problem
 urlpatterns = [
     path('',prob_home,name='prob_home'),
     path('create/',create_problem,name='create_problem'),
+    path('oldest/',prob_home,name='sort_oldest'),
+    path('unsolved/',prob_home,name='sort_unsolved'),
+    path('solved/',prob_home,name='sort_solved'),
+    path('tag/',prob_home,name='sort_tag'),
 ]
--- problems/views.py
+++ problems/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render,redirect
 from .forms import PostForm
 from django.http import JsonResponse
-from .models import Problem
+from .models import Problem,Tag
+from django.db.models import Q,Count
+
 # Create your views here.
 def prob_home(request):
+    # print(request.resolver_match.url_name)
     post_form = PostForm()
-    problems = Problem.objects.all().order_by('-created_on')
+    tags = Tag.objects.all().order_by('name')
+    
+
+    if request.resolver_match.url_name == 'sort_oldest':
+        problems = Problem.objects.all().order_by('created_on')
+    elif request.resolver_match.url_name == 'sort_unsolved':
+        problems = Problem.objects.filter(is_solved=False).order_by('-created_on')
+    elif request.resolver_match.url_name == 'sort_solved':
+        problems = Problem.objects.filter(is_solved=True).order_by('-created_on')
+    elif request.resolver_match.url_name == 'sort_tag':
+        tag_list = request.POST.getlist('tag_choice')
+        problems = Problem.objects.filter(tag__name__in=tag_list).order_by('-created_on').distinct()
+    else:
+        problems = Problem.objects.all().order_by('-created_on')
+
+
     context = {
         'post_form':post_form,
         'problems':problems,
+        'tags':tags,
     }
     return render(request,"problems/index.html",context)
 
@@ -23,3 +42,20 @@ def create_problem(request):
             return redirect('prob_home') 
         else:
             return redirect('prob_home')
+
+
+def sort_oldest(request):
+    post_form = PostForm()
+    problems = Problem.objects.all().order_by('created_on')
+    tags = Tag.objects.all().order_by('name')
+    context = {
+        'post_form':post_form,
+        'problems':problems,
+        'tags':tags,
+    }
+    return render(request,"problems/index.html",context)
+
+
+
+
+
